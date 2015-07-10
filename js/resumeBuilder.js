2d000454fd1abc32f823c87d08c59e9af4746f03
Joel Armando <joelarma@gmail.com>
2015-07-10 06:44:38 +0100

    Add bio, education, work and projects objects to resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,137 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	name : "Joel Armando",
+	role : "Educational Technologist",
+	contacts: {
+		"mobile": "+447824477292",
+		"email": "joelarma@gmail.com",
+		"github": "JoelArmandoP",
+		"location": "London"
+	},
+	pictureUrl: "images/joel.png",
+	welcomeMsg: "welcome",
+	"skills": ["learning design", "curriculum design", "moodle","e-learning","teaching"]
+
+};
+
+
+var education = {
+	"schools": [
+		{
+			"name": "Birkbeck College, University of London",
+			"city" : "London, UK",
+			"degree" : "MSc",
+			"Major" :  "Learning Technologies",
+			"year" : "2016"
+		},
+		{
+			"name": "Universidad de Santiago de Compostela",
+			"city" : "Santiago de Compostela, Spain",
+			"degree" : "PhD",
+			"Major" :"Education",
+			"year" : "2015"
+		},
+		{
+			"name": "Universidad Nacional de Córdoba",
+			"city" : "Córdoba, Argentina",
+			"degree" : "Master",
+			"Major" : "Educational Research",
+			"year" : "2009"
+		},
+		{
+			"name": "Universidad Nacional de Córdoba",
+			"city" : "Córdoba, Argentina",
+			"degree" : "BSc",
+			"Major" : "Educational Sciences",
+			"year" : "2000"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Front-end nanodegree",
+			"school": "Udacity",
+			"dates": "2015-2016",
+			"url": "http://www.udacity.com"
+		}
+	]
+};
+
+var work = {
+	"jobs": [
+		{
+			"employer": "City University London",
+			"title" : "Educational Technologist",
+			"location" : "London, UK",
+			"dates" : "May 2015- continue"
+			"description" :  "Educational Technologist. Learning, 
+							Enhancement and Development Department, based at Cass School of Business",			
+		},
+		{
+			"employer": "LamMovil",
+			"title" : "E-learning Consultant",
+			"location" : "Working from home",
+			"dates" : "Jan 2014 - Apr 2015"
+			"description" :"Provide educational advice in e-learning for healthcare programs.",
+			
+		},
+		{
+			"employer": "Universidad Nacional de Córdoba. School of Philosophy and Humanities",
+			"title" : "Director of the Educational Technology Department",
+			"location" : "Córdoba, Argentina",
+			"dates" : "Feb 2006 - Jun 2013"
+			"description" : "Led the Educational Technology Department, which provides infrastructure, 
+								services, training and advice to academic staff.
+								Responsabilities included: 
+								Led the design and integration of diverse technological tools 
+								(VLE, social media, video recording, digital and printed materials). 
+								Taught undergraduate and postgraduate courses and seminars on Design in 
+								Learning Technologies, Education and Audiovisual Media, New Tech­nologies 
+								in Teacher's Education, Curriculum Design and Development.
+								Co-directed research efforts sponsored by the University and took part 
+								in international research collaborations.",
+			
+		},
+		{
+			"employer": "Judicial Power, Córdoba Goverment",
+			"title" : "Executive Coordinator of Centro de Perfeccionamiento R. Núñez",
+			"location" : "Córdoba, Argentina",
+			"dates" : "Feb 2012 - Jun 2013"
+			"description" : "Executive in charge of the education programme for the Judicial Power's Learning 
+							& Development area. 
+							Designed the yearly education plan for more than 5000 judges, officials and employees 
+							and coordinated its execution.
+							Designed and led the deployment of the Center's Virtual Campus (moodle).
+							Coordinated the production of educational materials, and provided pedagogical advice.
+							Managed a multi-disciplinary 9-person team.",
+		}
+	]
+
+};
+
+var projects = {
+	"projects": [ 
+	{
+		"title": "Ansenuza, a digital repository of teaching materials for teachers' education",
+		"dates": "2010-2013",
+		"description": "Key activities: Liaison with external and internal stakeholders: Cordoba government representatives, 
+		IT and library UNC Departments, Teacher Training Institutions.
+		Evaluation and selection of software.
+		Design of licensing, accessibility, and user management policies.
+		Management of a 5-person multidisciplinary team for the deployment of the project.",
+		"images": "images/Joel.png"
+	},
+	{
+		"title": "Enhancing University Teaching with New Technology (EUTNT)",
+		"dates": "2000- 2012",
+		"description": "Research-based design of the initial project. Constant monitoring, 
+		evaluation and annual re-elaboration.
+		Periodic report to Spanish coordinator and report writing for AECID. 
+		Liaison with internal and external stakeholders in the development of 4 programmes: 
+		postgraduate teaching, research, equipment provisioning and production of educational materials.
+		Team leadership",
+		"images": "images/Joel.png"
+	}
+	]
+
+};
+
+
